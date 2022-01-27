ef36d0068a11d3eba2165aebe387c2a06a41b7fd
Erik Oien <oieeri26@uw.edu>
2022-01-27 11:05:51 -0800

    removed : from options

--- schema/sprout/sprout_fdw_config.sql
+++ schema/sprout/sprout_fdw_config.sql
@@ -7,16 +7,16 @@ CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 CREATE SERVER IF NOT EXISTS sprout_foreign_prod_db
         FOREIGN DATA WRAPPER postgres_fdw
         OPTIONS (
-            host :'sprout_db_server', 
-            port :'sprout_db_port', 
-            dbname :'sprout_db_name'
+            host 'sprout_db_server', 
+            port 'sprout_db_port', 
+            dbname 'sprout_db_name'
         );
 
 CREATE USER MAPPING IF NOT EXISTS FOR postgres
         SERVER sprout_foreign_prod_db
         OPTIONS (
-            user :'sprout_db_user', 
-            password :'sprout_db_pwd'
+            user 'sprout_db_user', 
+            password 'sprout_db_pwd'
         );
 
 IMPORT FOREIGN SCHEMA public 
