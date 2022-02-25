e6145748c9ee6e20ac93014683d0eab0ed3c2e7d
Erik Oien <oieeri26@uw.edu>
2022-02-25 14:28:28 -0800

    updated with feedback

--- schema/curated/visitation_referral.sql
+++ schema/curated/visitation_referral.sql
@@ -13,10 +13,25 @@ WITH org AS (
 	SELECT DISTINCT id
 	FROM staging."ServiceReferrals" sr
 	WHERE id NOT IN (SELECT DISTINCT id FROM sr)
+), dtpr AS (
+	/* 
+	use the id and org id (visit coord) to get dt_provider_received from this date 
+	when did it go to vc id to a provider id REMEMBER org id may not be provider
+	NEED first row provider with a provider and previous row was a VC (maybe routing org)
+	-- WHICH PROVIDER DO THEY WANT? might not be able to get the data
+	*/
+	SELECT "organizationId",
+		id,
+		MIN("createdAt") AS dt_provider_received
+	FROM staging."ServiceReferrals"
+	WHERE id = 58275
+	GROUP BY "organizationId",
+		id
+HAVING MAX(CASE WHEN "deletedAt" IS NOT NULL THEN 1 ELSE 0 END) = 0
 ), srts AS (
 	SELECT "ServiceReferralId" AS id,
-		MIN(CASE WHEN "StageTypeId" = 7 THEN "date" END) AS dt_referral_received,
-		MIN(CASE WHEN "StageTypeId" = 8 THEN "date" END) AS dt_provider_decision,
+		MIN(CASE WHEN "StageTypeId" = 7 THEN "createdAt" END) AS dt_referral_received,
+		MIN(CASE WHEN "StageTypeId" = 8 THEN "timestamp" END) AS dt_provider_decision, -- with stage 12 reject/accept max timestamp for 8 or 12
 		MIN(CASE WHEN "StageTypeId" = 9 THEN "date" END) AS dt_first_visit_scheduling_confirmation,
 		MAX(CASE WHEN "StageTypeId" = 9 THEN "date" END) AS dt_final_visit_scheduling_confirmation,
 		MIN(CASE WHEN "StageTypeId" = 10 THEN "date" END) AS dt_first_visit_scheduled,
@@ -37,32 +52,32 @@ SELECT sr.id AS id_visitation_referral,
 	"dshsOffice" AS office,
 	"socialWorkerId" AS id_worker,
 	CONCAT("socialWorkerLastName", ', ', "socialWorkerFirstName") AS worker_name,
-	NULL id_visit_coodinator,
+	NULL id_visit_coodinator, -- look in timeline stages for user id transition from visit coordinator to provider
 	"routingOrganizationId" AS id_routing_organization,
-	ro.name AS routing_organization_name,
-	NULL AS visit_coodinator_name,
+	ro.name AS routing_organization_name, 
+	NULL AS visit_coodinator_name, -- look in timeline stages for user id transition from visit coordinator to provider
 	"organizationId" AS id_provider_sprout,
 	po.name AS provider_name_sprout,
 	regexp_replace(po."famlinkId", '\D', '', 'g') AS id_pvdr_org,
-	NULL AS cd_referral_reason,
+	NULL AS cd_referral_reason, -- case statement off of "referralReason"
 	"referralReason" AS referral_reason,
 	dt_referral_received, -- check
 	"startDate" AS dt_start, -- check
 	"endDate" AS dt_end, -- check
-	NULL AS dt_provider_received, --check
+	NULL AS dt_provider_received, -- need to get first record of provider with sr.id 
 	dt_provider_decision,
-	NULL AS dt_intake, -- not sure what this is
-	NULL AS intake_time, -- not sure what this is
+	"intakeDateCompleted" AS dt_intake, 
+	"intakeTotalTime" AS intake_time, 
 	schedule_attempt_count, -- not sure if logic is correct
-	dt_final_visit_scheduling_confirmation, -- not sure if logic is correct
-	dt_first_visit_scheduling_confirmation, -- not sure if logic is correct
+	dt_final_visit_scheduling_confirmation,
+	dt_first_visit_scheduling_confirmation, 
 	dt_first_visit_scheduled,
-	NULL AS dt_first_visit_occurred,
+	NULL AS dt_first_visit_occurred, -- first attended visit "reportType" = parent or sibling do they care about sibling visits?
 	dt_referral_provider_resolved,
-	NULL AS dt_referral_closed,
-	NULL AS Parent_Count,
-	NULL AS Child_Count,
-	NULL AS CD_Provider_Decision,
+	NULL AS dt_referral_closed, -- min date between dt_referral_provider_resolved and dt_end
+	NULL AS Parent_Count, -- parentGaurdian? not sure
+	NULL AS Child_Count, -- childDetials?
+	NULL AS CD_Provider_Decision, -- get from provider decision
 	CASE WHEN rejected IS NULL THEN 'Accepted'
 		ELSE 'Rejected' 
 		END AS provider_decision,
@@ -72,10 +87,10 @@ SELECT sr.id AS id_visitation_referral,
 	CASE WHEN "visitTransportationType" IN ('Transportation Only', 'With Transportation') THEN 1
 		ELSE 0
 		END AS FL_Transport_Required,
-	NULL AS FL_Safety_Issue_Anger_Outburst,
-	NULL AS FL_Safety_Issue_Inappropriate_Conversation,
-	NULL AS FL_Safety_Issue_No_Contact_Order,
-	NULL AS FL_Safety_Issue_DV,
+	NULL AS FL_Safety_Issue_Anger_Outburst, -- safetyIssues
+	NULL AS FL_Safety_Issue_Inappropriate_Conversation, -- safetyIssues
+	NULL AS FL_Safety_Issue_No_Contact_Order, -- safetyIssues
+	NULL AS FL_Safety_Issue_DV, -- safetyIssues
 	CASE WHEN "levelOfSupervision" = 'Unsupervised' THEN 1
 		WHEN "levelOfSupervision" = 'Monitored' THEN 2
 		WHEN "levelOfSupervision" = 'Unsupervised' THEN 3
