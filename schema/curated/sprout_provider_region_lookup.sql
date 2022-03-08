8774b990c7072be5ad957c40825e8d6d664fdadd
Erik Oien <oieeri26@uw.edu>
2022-03-08 14:21:55 -0800

    updated table with regions from referrals

--- schema/curated/sprout_provider_region_lookup.sql
+++ schema/curated/sprout_provider_region_lookup.sql
@@ -1,10 +1,21 @@
+WITH org_region AS (
+	SELECT DISTINCT "regionId", 
+		"organizationId"
+	FROM staging."ServiceReferrals" AS sr
+	WHERE sr."deletedAt" IS NULL
+		AND sr."isCurrentVersion"
+		AND "formVersion" = 'Ingested'
+)
 SELECT o.id AS sprout_provider_id,
-	"regionId" AS cd_region,
-	CONCAT('Region ', "regionId") AS region,
+	COALESCE(org_reg."regionId", o."regionId") AS cd_region,
+	CONCAT('Region ', COALESCE(org_reg."regionId", o."regionId")) AS region,
 	NULL AS cd_office,
 	NULL AS office
 FROM staging."Organizations" AS o
 JOIN staging."OrganizationContracts" AS oc 
     ON o.id = oc."contractedOrganizationId"
 	AND oc."contractOwnerId" = 21
-ORDER BY o.id
+LEFT JOIN org_region AS org_reg 
+	ON o.id = org_reg."organizationId"
+ORDER BY o.id,
+	cd_region
