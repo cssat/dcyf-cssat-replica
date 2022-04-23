bc6def156c88f891c96895acc6057f2cf8967461
Gregor Thomas <gregor.p.thomas@gmail.com>
2022-04-22 20:56:24 -0400

    Update sprout_provider_county_lookup.sql

--- schema/curated/sprout_provider_county_lookup.sql
+++ schema/curated/sprout_provider_county_lookup.sql
@@ -2,18 +2,15 @@ WITH counties AS (
 	SELECT "organizationId", 
 		"countyId" AS cd_county,
 		co."name" AS county
-	FROM staging."OrganizationCounties" AS oc
-	JOIN staging."Counties" AS co
+	FROM replica."OrganizationCounties" AS oc
+	JOIN replica."Counties" AS co
 		ON oc."countyId" = co.id
 )
-SELECT o.id AS sprout_provider_id,
+SELECT o.id AS id_provider_sprout,
 	cd_county,
 	county
-FROM staging."Organizations" AS o
-JOIN staging."OrganizationContracts" AS oc 
-    ON o.id = oc."contractedOrganizationId"
-	AND oc."contractOwnerId" = 21
-JOIN counties AS co
+FROM dcyf."organizations" AS o
+INNER JOIN counties AS co
 	ON o.id = co."organizationId"
 ORDER BY o.id,
 	cd_county
