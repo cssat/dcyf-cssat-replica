364f869cc7b1195574c2b11b7876335706ba80be
Gregor Thomas <gregor.p.thomas@gmail.com>
2022-04-22 21:10:25 -0400

    Update sprout_provider_office_lookup.sql

--- schema/curated/sprout_provider_office_lookup.sql
+++ schema/curated/sprout_provider_office_lookup.sql
@@ -1,20 +1,21 @@
-WITH offices AS (
-	SELECT DISTINCT "organizationId", 
-		SUBSTRING("dshsOffice",'\(([0-9].*)\)')::INT AS cd_office,	
-		"dshsOffice" AS office
-	FROM staging."ServiceReferrals" AS sr
-	WHERE sr."deletedAt" IS NULL
-		AND sr."isCurrentVersion"
+WITH offices1 AS (
+	SELECT DISTINCT "organizationId", "dshsOffice"
+	FROM dcyf."service_referrals" AS sr
+	WHERE  sr."isCurrentVersion"
 		AND "formVersion" = 'Ingested'
-)	
-SELECT o.id AS sprout_provider_id,
+),
+offices AS (
+  SELECT DISTINCT 
+	"organizationId",
+	SUBSTRING("dshsOffice",'\(([0-9].*)\)')::INT AS cd_office,	
+		"dshsOffice" AS office
+	FROM offices1
+)
+SELECT o.id AS id_provider_sprout,
 	cd_office,
 	office
-FROM staging."Organizations" AS o
-JOIN staging."OrganizationContracts" AS oc 
-    ON o.id = oc."contractedOrganizationId"
-	AND oc."contractOwnerId" = 21
-LEFT JOIN offices AS offi
+FROM dcyf."organizations" AS o
+INNER JOIN offices AS offi
 	ON o.id = offi."organizationId"
-ORDER BY sprout_provider_id,
+ORDER BY id_provider_sprout,
 	cd_office
