5649e0a0e7d88637c2afc45ae3c4a87375942522
Gregor Thomas <gregort@uw.edu>
2022-04-22 21:53:55 -0400

    Update sprout_provider_region_lookup.sql

--- schema/curated/sprout_provider_region_lookup.sql
+++ schema/curated/sprout_provider_region_lookup.sql
@@ -1,20 +1,13 @@
 WITH org_region AS (
-	SELECT DISTINCT "regionId", 
-		"organizationId"
-	FROM staging."ServiceReferrals" AS sr
-	WHERE sr."deletedAt" IS NULL
-		AND sr."isCurrentVersion"
-		AND "formVersion" = 'Ingested'
-)
-SELECT o.id AS sprout_provider_id,
-	COALESCE(org_reg."regionId", o."regionId") AS cd_region,
-	CONCAT('Region ', COALESCE(org_reg."regionId", o."regionId")) AS region
-FROM staging."Organizations" AS o
-JOIN staging."OrganizationContracts" AS oc 
-    ON o.id = oc."contractedOrganizationId"
-	AND oc."contractOwnerId" = 21
-LEFT JOIN org_region AS org_reg 
-	ON o.id = org_reg."organizationId"
-WHERE org_reg."regionId" != 7
-ORDER BY o.id,
-	cd_region
+         SELECT DISTINCT sr."regionId",
+            sr."organizationId"
+           FROM dcyf.service_referrals sr
+          WHERE sr."isCurrentVersion" AND sr."formVersion"::text = 'Ingested'::text
+        )
+ SELECT o.id AS id_provider_sprout,
+    COALESCE(org_reg."regionId", o."regionId") AS cd_region,
+    concat('Region ', COALESCE(org_reg."regionId", o."regionId")) AS region
+   FROM dcyf.organizations o
+     JOIN org_region org_reg ON o.id = org_reg."organizationId"
+  WHERE org_reg."regionId" <> 7
+  ORDER BY o.id, (COALESCE(org_reg."regionId", o."regionId"))
